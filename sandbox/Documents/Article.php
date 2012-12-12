8d19bbf9042b13d5d9b103e2712db3a2278ba515
Alistair MacDonald <alistair.macdonald@illumina.co.uk>
2012-12-12 10:58:00 +0000

    Create a sandbox with a very basic structure

---
+++ sandbox/Documents/Article.php
@@ -0,0 +1,76 @@
+<?php 
+
+namespace Documents;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @XmlRootEntity(xml="article")
+ */
+class Article
+{
+    /**
+     * @XmlId
+     * @XmlField(type="string", node="attribute")
+     */
+    public $id;
+    
+    /**
+     * @XmlText(type="string", required=true)
+     */
+    public $name;
+    
+    /*
+     * @EmbedMany(targetDocument="Documents\Section")
+     * 
+     */
+    /**
+     * @XmlElement(type="Documents\Section", collection="true", wrapper="sections")
+     */
+    public $sections;
+    
+    
+    public function __construct()
+    {
+        $this->sections = new ArrayCollection();
+    }
+    
+    public function setName($name)
+    {
+        $this->name = $name;
+        
+        return $this;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    
+    public function getSections()
+    {
+        return $this->sections;
+    }
+    
+    public function addSection(Section $section)
+    {
+        $this->sections[] = $section;
+    }
+    
+    public function removeSection(Section $section)
+    {
+        $this->sections->remove($section);
+    }
+}
